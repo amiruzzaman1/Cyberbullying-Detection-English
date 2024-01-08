bcc657f186fd408b99262c1ede5b00bda5c964f3
Amiruzzaman <amirzaman24@gmail.com>
2024-01-08 20:23:01 +0600

    Update CyberBullyingEnglish.py

--- CyberBullyingEnglish.py
+++ CyberBullyingEnglish.py
@@ -13,6 +13,7 @@ with open(vectorizer_filename, 'rb') as vectorizer_file:
 
 # Define a function to predict cyberbullying and filter bad words
 def predict_cyberbullying_and_filter_bad_words(text):
+    text = text.lower()
     # Vectorize the input text using the loaded vectorizer
     text_tfidf = vectorizer.transform([text])
     
@@ -25,16 +26,16 @@ def predict_cyberbullying_and_filter_bad_words(text):
     
     # List of cyberbullying words
     cyberbullying_words = ["bitch", "fuck", "asshole", "shitty", "ass", "slut", "cunt", "motherfucker", "wanker", "dick", "shit", "bastard",
-                       "pissed off", "arse", "bugger", "bloody", "whore", "anal", "anus", "ar5e", "arrse", "assbag", "assbandit", 
+                       "pissed off", "arse", "bugger", "bloody", "whore", "anal", "anus", "arse", "arrse", "assbag", "assbandit", 
                        "assbanger", "assbite", "assclown", "asscock", "asscracker", "asses", "assface", "assfuck", "assfucker",
                        "assfukka", "assgoblin", "asshat", "asshead", "asshole", "assholes", "asshopper", "assjacker", "asslick", 
                        "asslicker", "assmonkey", "assmunch", "assmuncher", "assnigger", "asspirate", "assshit", "assshole", "asssucker", 
-                       "asswad", "asswhole", "asswipe", "auto erotic", "autoerotic", "axwound", "b!tch", "b00bs", "b17ch", "b1tch", "ballbag", 
+                       "asswad", "asswhole", "asswipe", "auto erotic", "autoerotic", "asswound", "boobs", "bitch", "b1tch", "ballbag", 
                        "balls", "ballsack", "bampot", "bangbros", "bareback", "barely legal", "barenaked", "bastard", "bastardo", "bastinado", 
                        "bbw", "bdsm", "bitches", "bitchin", "bitching", "bitchtits", "bitchy", "blowjob", "boob", "boobs", "booobs", "boooobs",
                        "booooobs", "booooooobs", "brotherfucker", "bumblefuck", "bung hole", "buttcheeks", "buttfucka", 
-                       "buttfucker", "butthole", "buttmuch", "buttplug", "c0ck", "c0cksucker", "camgirl", "camslut", 
-                       "camwhore", "circlejerk", "cl1t", "cleveland steamer", "clit", "clitface", "clitfuck", "clitoris", 
+                       "buttfucker", "butthole", "buttmuch", "buttplug", "cock", "cocksucker", "camgirl", "camslut", 
+                       "camwhore", "circlejerk", "clit", "cleveland steamer", "clit", "clitface", "clitfuck", "clitoris", 
                        "clits", "clover clamps", "clusterfuck", "cockass", "cockbite", "cockburger", "cockeye", "cockface",
                        "cockfucker", "cockhead", "cockjockey", "cockknoker", "cocklump", "cockmaster", "cockmongler",
                        "cockmongruel", "cockmonkey", "cockmunch", "cockmuncher", "cocknose", "cocknugget", "cocks",
@@ -52,8 +53,8 @@ def predict_cyberbullying_and_filter_bad_words(text):
                        "doochbag", "double dong", "penetration", "doublelift", "douche", "douchebag", "dumbass",
                        "dumbcunt", "dumbfuck", "ejaculate", "ejaculated", "ejaculates", "ejaculating", "ejaculatings",
                        "ejaculation", "ejakulate", "erotism", "eunuch", "f u c k", "fagfucker", "fagging", "faggit",
-                       "faggitt", "faggot", "faggotcock", "fanny", "fannyflaps", "fannyfucker", "fanyy", "fatass", "fcuk", 
-                       "fcuker", "fcuking", "fecal", "feck", "fecker", "felch", "felching", "fellate", "fellatio", "feltch",
+                       "faggitt", "faggot", "faggotcock", "fanny", "fannyflaps", "fannyfucker", "fanyy", "fatass", "fuck", 
+                       "fucker", "fucking", "fecal", "feck", "fecker", "felch", "felching", "fellate", "fellatio", "feltch",
                        "female squirting", "femdom", "figging", "fingerbang", "fingerfuck", "fingerfucked", "fingerfucker",
                        "fingerfuckers", "fingerfucking", "fingerfucks", "fingering", "fistfuck", "fistfucked", "fistfucker", 
                        "fistfuckers", "fistfucking", "fistfuckings", "fistfucks", "fisting", 
@@ -62,18 +63,18 @@ def predict_cyberbullying_and_filter_bad_words(text):
                        "fuckings", "fucking shit motherfucker", "fuckme", "fucknut", "fucknutt", "fuckoff", "fucks",
                        "fuckstick", "fucktard", "fucktards", "fucktart", "fucktwat", "fuckup", "fuckwad", "fuckwhit", 
                        "fuckwit", "fuckwitt", "fudge packer", "fudgepacker", "fuk", "fuker", "fukker", "fukkin", "fuks", 
-                       "fukwhit", "fukwit", "futanari", "fux", "fux0r", "g-spot", "gangbang", "gangbanged", "gangbangs",
+                       "fukwhit", "fukwit", "futanari", "fux", "fuxor", "g-spot", "gangbang", "gangbanged", "gangbangs",
                        "gayass", "gaybob", "gaydo", "gayfuck", "gayfuckist", "goregasm", "handjob", "hard core", "hardcore",
                        "hardcoresex", "hooker", "arse", "ass fuck", "ass hole", "assfucker", "asshole", "assshole", "bastard",
                        "fucking bitch", "cock", "bloody hell", "boong", "cockfucker", "cocksuck", "coon", "cyberfuck",
                        "erection", "erotic", "faggot fuck", "fuck off", "fuck you", "fuckass", "fuckhole", "hardcore", 
                        "lesbian", "lesbians", "motherfuck", "negro", "nigger", "orgasim", "orgasm", "penis", "penisfucker", 
                        "piss", "piss off", "pussy", "sexy shit", "sexy slut", "son of a bitch", "suck tits", "xxx", 
-                       "kill yourself", "fuck yourself", "beheading"]
+                       "kill yourself", "fuck yourself", "beheading", "terrorist"]
     
     # Check for and filter out bad words from the text
     for word in cyberbullying_words:
-        if word in text.lower():
+        if word.lower() in text:
             filtered_text = filtered_text.replace(word, '*' * len(word))
             bad_words.append(word)
     
@@ -99,6 +100,18 @@ if st.button("Predict"):
         if bad_words:
             st.write(f"Bad Words: {', '.join(bad_words)}")
         else:
-            st.write("No bad words found.")
-        st.write("Filtered Text:")
-        st.write(f"<span style='color:red; font-weight:bold'>{filtered_text}</span>", unsafe_allow_html=True)
+            st.write("<span style='color:cyan;'>No bad words found.</span>", unsafe_allow_html=True)
+        if bad_words:
+            st.write("Filtered Text:")
+            st.write(f"<span style='color:red; font-weight:bold'>{filtered_text}</span>", unsafe_allow_html=True) 
+        else:
+            st.write("Original Text:")
+            st.write(f"{filtered_text}", unsafe_allow_html=True)
+
+
+st.header("Sample Texts")
+st.write("It's always the filthy " + "<span style='color:red; font-weight:bold'>bitch</span> that creates problem between us", unsafe_allow_html=True)
+st.write("Do you believe it is appropriate to refer to a Muslim as a " + "<span style='color:red; font-weight:bold'>terrorist</span>?", unsafe_allow_html=True)
+st.write("I hope you're doing well and having a great day. Let's catch up soon! 😊")
+st.write("The team's score is disgraceful.")
+
